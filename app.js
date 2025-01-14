3323130eb57722f76390608fd8b1b90edb7466b1
Brijesh Kori <73524943+Brijesh-09@users.noreply.github.com>
2025-01-14 15:49:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors({
     // origin: ['https://khel-fron.vercel.app', 'http://localhost:5173/'],
-    origin: ['https://khel-fron.vercel.app', 'http://localhost:5173'], // Allow requests from this frontend origin
+    origin: ['https://khel-fron-eta.vercel.app', 'http://localhost:5173'], // Allow requests from this frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     credentials: true, // Allow cookies if necessary
 }));
